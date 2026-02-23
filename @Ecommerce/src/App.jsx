4dfd68625ac0268a7aab0443d9d8ba432ee87ea6
crazyharisri <crazysolomon03@gmail.com>
2026-02-23 14:46:33 +0530

    new

--- @Ecommerce/src/App.jsx
+++ @Ecommerce/src/App.jsx
@@ -3,4 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function App() {
+    return(
+        <h1>this is web page</h1>
+    ) 
+}
 
+export default App

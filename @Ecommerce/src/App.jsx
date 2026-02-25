ee84847fc7a850405c72292171835a3e45f76d69
Preethi-ravi-29 <preethiravichandran@gmail.com>
2026-02-25 14:45:05 +0530

    contact page

--- @Ecommerce/src/App.jsx
+++ @Ecommerce/src/App.jsx
@@ -2,21 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import contact from './pages/contact'
+import Contact from './pages/contact.jsx'
 
 
 function App() {
-<<<<<<< HEAD
-  const [count, setCount] = useState(0)
+ 
 
   return (
-     contact()
+    <Contact />
   )
-=======
-    return(
-        <h1>this is web page</h1>
-    ) 
->>>>>>> 4dfd68625ac0268a7aab0443d9d8ba432ee87ea6
 }
 
 export default App
